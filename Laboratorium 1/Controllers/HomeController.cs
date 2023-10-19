2b4e1cf3c7a782826ab71354d01965083dee61da
sebastiangron <sebastiangron3@gmail.com>
2023-10-19 20:04:49 +0200

    Initial commit

---
+++ Laboratorium 1/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Laboratorium_1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Laboratorium_1.Controllers
+{
+    public enum Operators
+    {
+        ADD, SUB, MUL, DIV
+    }
+
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About([FromQuery(Name = "app-author")]string author) 
+        {
+            // string author = Request.Query["author"];
+            ViewBag.Author = author;
+            return View();
+        }
+
+        public IActionResult Calculator([FromQuery(Name = "operator")]Operators op, double? a, double? b)
+        {
+            if (a == null || b == null) { return View("Error"); }
+
+            switch (op)
+            {
+                case Operators.ADD: ViewBag.Result = a + b; break;
+                case Operators.SUB: ViewBag.Result = a - b; break;
+                case Operators.MUL: ViewBag.Result = a * b; break;
+                case Operators.DIV: ViewBag.Result = a / b; break;
+            }
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Laboratorium 2/Controllers/CalculatorController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Laboratorium_2.Models;
+
+namespace Laboratorium_2.Controllers
+{   
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Result([FromForm] CalculatorViewModel model)
+        {
+            if (!model.IsValid())
+            {
+                return View("Error");
+            }
+            return View(model);
+        }
+    }
+}
